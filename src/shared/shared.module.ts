f0e4816d07c9ef98b912bf3006efe020f018ab90
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2025-06-19 22:52:34 +0700

    feat: create shared module for combining modules used in multiple modules

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { HashingModule } from './hashing/hashing.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, HashingModule],
+  exports: [PrismaModule, HashingModule],
 })
 export class SharedModule {}
