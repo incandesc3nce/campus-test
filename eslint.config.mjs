a7351ef22b4b6d8a6ce2c3d4e8139b5756472e83
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2025-06-19 22:51:46 +0700

    tweak: disable no-unsafe-call rule
    the rule was throwing errors when used with class-validator and class-transformer libraries

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -29,6 +29,7 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/no-unsafe-call': 'off',
       'prettier/prettier': [
         'error',
         {
