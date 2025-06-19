50207904e8e4ac7ad8435107ed9c7ebcc35fc12c
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2025-06-20 00:07:42 +0700

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // setup
   app.setGlobalPrefix('v1');
   app.enableShutdownHooks();
+  app.enableCors();
+
+  // start the application
   await app.listen(process.env.PORT ?? 3000);
 }
 
