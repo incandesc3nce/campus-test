dfe7f484d081354085fb159251cbcf2f1bf34e55
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2025-06-20 00:25:23 +0700

    feat: import shared, auth and users modules in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), SharedModule, AuthModule, UsersModule],
+  providers: [],
 })
 export class AppModule {}
