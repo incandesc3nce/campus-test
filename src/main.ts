3c0200a5c9d58e9e137e652932c918535abc1040
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2025-06-19 11:38:36 +0700

    feat: enable shutdown lifecycle hooks

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('v1');
+  app.enableShutdownHooks();
   await app.listen(process.env.PORT ?? 3000);
 }
 
