be40545177fdc93252621f67b232aa3c28680032
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2025-06-20 00:06:27 +0700

    feat: create auth module

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,28 @@
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
+
+@Controller('auth')
+@UsePipes(new ValidationPipe({ transform: true, whitelist: true }))
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('login')
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
+
+  @Post('register')
+  @HttpCode(201)
+  register(@Body() registerDto: RegisterDto) {
+    return this.authService.register(registerDto);
+  }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,48 @@
+import { UsersService } from '@/users/users.service';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { LoginDto } from './dto/login.dto';
+import { HashingService } from '../shared/hashing/hashing.service';
+import { RegisterDto } from './dto/register.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+    private hashingService: HashingService,
+  ) {}
+
+  async login(loginDto: LoginDto): Promise<{ accessToken: string }> {
+    const user = await this.usersService.findOneByEmail(loginDto.email);
+
+    const isValidPassword = await this.hashingService.verifyPassword(
+      loginDto.password,
+      user.passwordHash,
+    );
+
+    if (!isValidPassword) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+
+    const payload = { sub: user.id, email: user.email };
+
+    return {
+      accessToken: await this.jwtService.signAsync(payload),
+    };
+  }
+
+  async register(registerDto: RegisterDto): Promise<{ accessToken: string }> {
+    const newUser = await this.usersService.createUser({
+      email: registerDto.email,
+      name: registerDto.name,
+      password: registerDto.password,
+    });
+
+    const payload = { sub: newUser.id, email: newUser.email };
+
+    return {
+      accessToken: this.jwtService.sign(payload),
+    };
+  }
+}
