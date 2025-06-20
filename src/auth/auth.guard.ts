3d851667b62c7b57ca9036261442479b74c72e72
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2025-06-20 16:00:06 +0700

    feat: create auth guard to protect routes

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,42 @@
+import { JwtPayload } from '@/shared/types/JwtPayload';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') || [];
+    return type === 'Bearer' ? token : undefined;
+  }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const req: Request & { tokenPayload?: JwtPayload } = context
+      .switchToHttp()
+      .getRequest<Request>();
+    const token = this.extractTokenFromHeader(req);
+
+    if (!token) {
+      throw new UnauthorizedException('You must be logged in to access this resource');
+    }
+
+    try {
+      const payload: JwtPayload = await this.jwtService.verifyAsync(token, {
+        secret: process.env.JWT_SECRET,
+      });
+
+      req['tokenPayload'] = payload;
+    } catch {
+      throw new UnauthorizedException('Invalid token');
+    }
+
+    return true;
+  }
+}
