e83d4a0e046800d5d823c9dc8591fed16e23fef0
incandesc3nce <62383010+incandesc3nce@users.noreply.github.com>
2025-06-21 00:10:06 +0700

    refactor: remove unused userId property

--- src/tasks/dto/taskResponse.dto.ts
+++ src/tasks/dto/taskResponse.dto.ts
@@ -2,7 +2,6 @@ import { TaskStatus } from 'generated/prisma';
 
 export class TaskResponseDto {
   id: string;
-  userId?: undefined;
   title: string;
   description: string | null;
   status: TaskStatus;
